0f7a297fb3abe43516c51172c8547bfc06086e9c
Chiri Vulpes <yuudaari@gmail.com>
2021-05-06 11:18:16 +1000

    Add custom rendering of MmWw, improve k

--- ts/ui/element/Text.ts
+++ ts/ui/element/Text.ts
@@ -9,6 +9,7 @@ const CHAR_WIDTH = 6;
 const CHAR_HEIGHT = 9;
 
 enum FontSprite {
+	Wide,
 	Uppercase,
 	Lowercase,
 	Numbers,
@@ -22,28 +23,69 @@ enum FontSprite {
 	Minus,
 }
 
-interface IFontSpriteCharacterRange {
+interface IFontSpriteDefinition {
+	width?: number;
+}
+
+interface IFontSpriteCharacterRange extends IFontSpriteDefinition {
 	start: number;
 	end: number;
 }
 
-type FontSpriteDefinition = IFontSpriteCharacterRange | number;
+interface IFontSpriteCodeList extends IFontSpriteDefinition {
+	codes: number[];
+}
+
+type FontSpriteDefinition = IFontSpriteCharacterRange | IFontSpriteCodeList | number;
+
+function char (...chars: string[]): FontSpriteDefinition;
+function char (width: number, ...chars: string[]): FontSpriteDefinition;
+function char (width?: number | string, ...chars: string[]) {
+	if (typeof width === "string")
+		chars.unshift(width), width = undefined;
+
+	const codes = chars.map(char => char.charCodeAt(0));
+	if (codes.length === 1)
+		return codes[0];
+
+	return { codes, width };
+}
+
+function charRange (start: string, end: string) {
+	return { start: start.charCodeAt(0), end: end.charCodeAt(0) };
+}
+
+function getSpriteIndexOffset (def: FontSpriteDefinition, code: number) {
+	if (typeof def === "number")
+		return 0;
+
+	const width = def.width ?? CHAR_WIDTH;
+	if ("codes" in def)
+		return def.codes.indexOf(code) * width;
+
+	return (code - def.start) * width;
+}
 
 const fontSpriteDefinitions: Record<FontSprite, FontSpriteDefinition> = {
-	[FontSprite.Uppercase]: { start: "A".charCodeAt(0), end: "Z".charCodeAt(0) },
-	[FontSprite.Lowercase]: { start: "a".charCodeAt(0), end: "z".charCodeAt(0) },
-	[FontSprite.Numbers]: { start: "0".charCodeAt(0), end: "9".charCodeAt(0) },
-	[FontSprite.Parentheses]: { start: "(".charCodeAt(0), end: ")".charCodeAt(0) },
-	[FontSprite.Period]: ".".charCodeAt(0),
-	[FontSprite.Comma]: ",".charCodeAt(0),
-	[FontSprite.Exclamation]: "!".charCodeAt(0),
-	[FontSprite.Colon]: ":".charCodeAt(0),
-	[FontSprite.Currency]: "$".charCodeAt(0),
-	[FontSprite.Plus]: "+".charCodeAt(0),
-	[FontSprite.Minus]: "-".charCodeAt(0),
+	[FontSprite.Wide]: char(9, "M", "m", "W", "w"),
+	[FontSprite.Uppercase]: charRange("A", "Z"),
+	[FontSprite.Lowercase]: charRange("a", "z"),
+	[FontSprite.Numbers]: charRange("0", "9"),
+	[FontSprite.Parentheses]: charRange("(", ")"),
+	[FontSprite.Period]: char("."),
+	[FontSprite.Comma]: char(","),
+	[FontSprite.Exclamation]: char("!"),
+	[FontSprite.Colon]: char(":"),
+	[FontSprite.Currency]: char("$"),
+	[FontSprite.Plus]: char("+"),
+	[FontSprite.Minus]: char("-"),
 };
 
 const characterWidthExceptions: Partial<Record<string, number>> = {
+	M: 9,
+	m: 9,
+	W: 8,
+	w: 8,
 	I: 5,
 	T: 5,
 	i: 3,
@@ -136,16 +178,18 @@ export default class Text extends Element {
 		for (let i = 0; i < this.text.length; i++) {
 			const char = this.text[i];
 			const code = this.text.charCodeAt(i);
+			const width = characterWidthExceptions[char] ?? CHAR_WIDTH;
 			const fontSprite = this.getFontSprite(code);
 			if (fontSprite !== undefined) {
 				const sprite = Sprite.get(`ui/font/${FontSprite[fontSprite].toLowerCase()}`);
 				await sprite.loaded;
 				const def = fontSpriteDefinitions[fontSprite];
+				const index = getSpriteIndexOffset(def, code);
 				canvas.context.imageSmoothingEnabled = false;
-				sprite.render(canvas, x, y, CHAR_WIDTH * scale, CHAR_HEIGHT * scale, typeof def === "number" ? 0 : (code - def.start) * CHAR_WIDTH, 0, CHAR_WIDTH, CHAR_HEIGHT);
+				sprite.render(canvas, x, y, width * scale, CHAR_HEIGHT * scale, index, 0, width, CHAR_HEIGHT);
 			}
 
-			x += (characterWidthExceptions[char] ?? CHAR_WIDTH) * scale;
+			x += width * scale;
 		}
 
 		if (svg) {
@@ -158,7 +202,8 @@ export default class Text extends Element {
 		for (const fontSprite of Enums.values(FontSprite)) {
 			const definition = fontSpriteDefinitions[fontSprite];
 			const matches = typeof definition === "number" ? definition === char
-				: char >= definition.start && char <= definition.end;
+				: "codes" in definition ? definition.codes.includes(char)
+					: char >= definition.start && char <= definition.end;
 
 			if (matches)
 				return fontSprite;
