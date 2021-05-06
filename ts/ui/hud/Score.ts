8dccf55958872223947e700b082870682cb8988b
Chiri Vulpes <yuudaari@gmail.com>
2021-05-06 14:17:29 +1000

    Show only "Highest stock value" when on surface

--- ts/ui/hud/Score.ts
+++ ts/ui/hud/Score.ts
@@ -1,3 +1,4 @@
+import { GameState } from "Constants";
 import { EventBus } from "Events";
 import { Stats } from "game/Stats";
 import { SYMBOL_NEWLINE } from "ui/element/ContainerElement";
@@ -11,20 +12,26 @@ export default class ScoreElement extends FlowContainerElement {
 	public constructor (stats: Stats) {
 		super();
 		this
-			.add(new LabelledValue()
-				.setLabel("Depth")
-				.add(new Text(() => `${stats.turn}`)
-					.setRefreshOn(EventBus.Main, "update", () => this.valueChanged("turn", stats.turn)))
-				.add(new EphemeralElement(() => stats.scheduledDepthDifference)
-					.add(new Text(" (").setStyle("colour", Scheme.COLOUR_FOREGROUND_TERTIARY))
-					.add(new Text(() => `${stats.scheduledDepthDifference > 0 ? "+" : ""}${stats.scheduledDepthDifference}`)
-						.setRefreshOn(EventBus.Main, "update", () => this.valueChanged("depthDiff", stats.scheduledDepthDifference)))
-					.add(new Text(")").setStyle("colour", Scheme.COLOUR_FOREGROUND_TERTIARY))))
-			.add(SYMBOL_NEWLINE)
-			.add(new EphemeralElement(() => stats.turn * 10 !== stats.score)
+			.add(new EphemeralElement(() => stats.state === GameState.Surface)
 				.add(new LabelledValue()
-					.setLabel("Stock value")
-					.add(new Text(() => `$${stats.score}`)
-						.setRefreshOn(EventBus.Main, "update", () => this.valueChanged("score", stats.score)))));
+					.setLabel("Highest stock value")
+					.add(new Text(() => `$${stats.highscore}`)
+						.setRefreshOn(EventBus.Main, "update", () => this.valueChanged("highscore", stats.highscore)))))
+			.add(new EphemeralElement(() => stats.state !== GameState.Surface)
+				.add(new LabelledValue()
+					.setLabel("Depth")
+					.add(new Text(() => `${stats.turn}`)
+						.setRefreshOn(EventBus.Main, "update", () => this.valueChanged("turn", stats.turn)))
+					.add(new EphemeralElement(() => stats.scheduledDepthDifference)
+						.add(new Text(" (").setStyle("colour", Scheme.COLOUR_FOREGROUND_TERTIARY))
+						.add(new Text(() => `${stats.scheduledDepthDifference > 0 ? "+" : ""}${stats.scheduledDepthDifference}`)
+							.setRefreshOn(EventBus.Main, "update", () => this.valueChanged("depthDiff", stats.scheduledDepthDifference)))
+						.add(new Text(")").setStyle("colour", Scheme.COLOUR_FOREGROUND_TERTIARY))))
+				.add(SYMBOL_NEWLINE)
+				.add(new EphemeralElement(() => stats.turn * 10 !== stats.score)
+					.add(new LabelledValue()
+						.setLabel("Stock value")
+						.add(new Text(() => `$${stats.score}`)
+							.setRefreshOn(EventBus.Main, "update", () => this.valueChanged("score", stats.score))))));
 	}
 }
